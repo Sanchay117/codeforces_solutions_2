0274d077bd6f64da94191fc974505b866a1eebf6
Sanchay Singh <sanchay072@gmail.com>
2025-03-28 14:18:58 +0530

    another solved

---
+++ Lost_Numbers_1167B.cpp
@@ -0,0 +1,211 @@
+//
+// Created by bravefart69 on 28/3/25.
+//
+#include <bits/stdc++.h>
+#include <climits>
+#include <fstream>
+#include <future>
+#include <numeric>
+
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define ulli unsigned long long int
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+#define vi vector<int>
+#define vll vector<long long>
+#define vlli vector<long long int>
+#define pii pair<int,int>
+#define pll pair<long long int,long long int>
+#define mii map<int,int>
+#define mci map<char,int>
+#define mic map<int,char>
+#define msi map<string,int>
+#define mis map<int,string>
+#define all(arr) arr.begin(), arr.end()
+#define all(arr,n) arr, arr+n
+#define fr(i,a,b) for(int i = a; i<b;i++)
+#define bug(...) __f (#__VA_ARGS__, __VA_ARGS__)
+#define MOD 1000000007
+#define haan cout << "YES" << endl
+#define nahi cout << "NO" << endl
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define out(x) cout << x << endl
+#define tupiii tuple<int,int,int>
+#define billion 10e9
+#define printMatrix(arr,n,m) for(int i = 0;i<n;i++){\
+        for(int j = 0;j<m;j++){\
+            cout << arr[i][j] << " ";\
+        }\
+        cout << endl;\
+    }
+#define sort_dec(arr,n) sort(arr,arr+n,greater<>());
+#define sort_asc(arr,n) sort(arr,arr+n);
+#define printV(vec) for(int i = 0;i<vec.size();i++) cout << vec[i] << " ";\
+	cout << endl;
+#define MatrixInput(arr,n,m) for(int i = 0;i<n;i++){\
+                                for(int j = 0;j<m;j++){\
+                                    cin >> arr[i][j] ;\
+                                }\
+                             }
+#define ArrInput(arr,n) for(int i = 0;i<n;i++) cin >> arr[i];
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+#define fo(i,n)   for(i=0;i<(n);++i)
+#define repA(i,j,n)   for(i=(j);i<=(n);++i)
+#define repD(i,j,n)   for(i=(j);i>=(n);--i)
+#define all(x) begin(x), end(x)
+#define sz(x) ((lli)(x).size())
+#define eb emplace_back
+#define X first
+#define Y second
+
+// ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+
+const int MAXN = 10000000;  // Set the maximum value of n as 10 million
+std::vector<bool> primes(MAXN + 1, true);  // Create a bool vector of size 10,000,001
+
+void SieveOfEratosthenes() {
+    for (int p = 2; p * p <= MAXN; p++) {  // Iterate up to sqrt(MAXN)
+        if (primes[p]) {  // Check if p is a prime number
+            for (int i = p * p; i <= MAXN; i += p)  // Mark all multiples of p as false
+                primes[i] = false;
+        }
+    }
+}
+
+bool in(int a,initializer_list<int> arr) {
+    for(int x : arr) if(x==a) return true;
+    return false;
+}
+
+void bfs(vector<bool> &visited,vi adj[], vi& stored ,int start) {
+
+
+    int cnt = 0;
+
+    if(visited[start]) return;
+
+    queue<int> q;
+    q.push(start);
+
+    visited[start] = true;
+
+    vi res;
+
+    while(!q.empty()) {
+        int curr = q.front();
+        res.pb(curr);
+        q.pop();
+        cnt++;
+        for (int x : adj[curr]) {
+            if (!visited[x]) {
+                visited[x] = true;
+                q.push(x);
+            }
+        }
+    }
+
+    for(int x:res) {
+        stored[x] = max(cnt,stored[x]);
+    }
+
+
+}
+
+void solve(int tt) {
+
+    map<int,pii> map;
+
+    int arr[6] = {4,8,15,16,23,42};
+
+    fr(i,0,5) {
+        fr(j,i+1,6) {
+            map[arr[i]*arr[j]] = {arr[i],arr[j]};
+        }
+    }
+
+    vi ans(6);
+    cout << "? 1 2" << endl;
+    fflush(stdout);
+
+    int x;cin >> x;
+    cout << "? 2 3" << endl;
+    fflush(stdout);
+
+    int y;cin >> y;
+
+    if((map[x].F == map[y].F) || (map[x].F == map[y].S)) {
+        ans[1] = map[x].F;
+        ans[0] = map[x].S;
+        if(map[x].F == map[y].F) ans[2] = map[y].S;
+        else ans[2] = map[y].F;
+    }else {
+        ans[1] = map[x].S;
+        ans[0] = map[x].F;
+        if(map[x].S == map[y].F) ans[2] = map[y].S;
+        else ans[2] = map[y].F;
+    }
+
+    cout << "? 4 5" << endl;
+    fflush(stdout);
+
+    cin >> x;
+
+    cout << "? 5 6" << endl;
+    fflush(stdout);
+
+    cin >> y;
+
+    if((map[x].F == map[y].F) || (map[x].F == map[y].S)) {
+        ans[4] = map[x].F;
+        ans[3] = map[x].S;
+        if(map[x].F == map[y].F) ans[5] = map[y].S;
+        else ans[5] = map[y].F;
+    }else {
+        ans[4] = map[x].S;
+        ans[3] = map[x].F;
+        if(map[x].S == map[y].F) ans[5] = map[y].S;
+        else ans[5] = map[y].F;
+    }
+
+    cout << "! ";
+    printV(ans);
+
+}
+
+int32_t main() {
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    // cin >> t;
+    int i = 1;
+
+    while (t--) {
+        solve(i);
+        i++;
+    }
+
+    return 0;
+}
